251b171ef327b8915314320b4b4c012acee9b2a4
devang <dev.paliwal@gmail.com>
2013-03-22 01:45:41 +0530

    Added code to generate a books.html from sever

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('books.html');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(request, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(index);
 });
 
 var port = process.env.PORT || 5000;
